b0b4749136eebf8e94d1f525062824f8d7cd62a7
Server048 <64312413+Server048@users.noreply.github.com>
2021-05-08 05:46:47 +0800

    Update bagus.sh

--- bagus.sh
+++ bagus.sh
@@ -3,7 +3,10 @@
 apt update
 apt upgrade
 apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
 apt install cowsay -y
 apt install nano -y
 apt install ruby -y
 gem install lolcat
+figlet -f big Done !!! | lolcat
