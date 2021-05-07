61e025b0b340cb927bb92b25a0c2de6bfbc0ace5
Server048 <64312413+Server048@users.noreply.github.com>
2021-05-08 06:09:01 +0800

    Update bagus.sh

--- bagus.sh
+++ bagus.sh
@@ -12,7 +12,7 @@ apt install ruby -y
 gem install lolcat
 figlet -f big Done !!! | lolcat
 echo
-echo -e "\e[1m Now Run bash t-ban.sh...!!!"
+echo -e "\e[1m Xrig Coin By Server048"
 echo
-echo -e "\e[1m\e[32m Developed by :\e[33m Sutariya Parixit (8h4i)"
+echo -e "\e[1m\e[32m Developed by :\e[33m Server048 (8h4i)"
 z="
