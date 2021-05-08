cdaf498ec743f2354eb37e6a8507c6024acf2d31
Server048 <64312413+Server048@users.noreply.github.com>
2021-05-08 10:14:32 +0800

    Update baguscoin.sh

--- baguscoin.sh
+++ baguscoin.sh
@@ -1,5 +1,37 @@
 #!/sh
 
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo -e "\e[1m Xrig Coin By Server048"
+echo
+echo -e "\e[1m\e[32m Developed by :\e[33m Server048 (8h4i)"
+z="
+try:
+	
+	import mechanize, requests, random, sys, os, re
+	from time import sleep
+	from cookielib import LWPCookieJar as Cookie
+	from requests.exceptions import ConnectionError
+	os.system('clear')
+	def Tik(s):
+		for i in s + '\n':
+			sys.stdout.write(i)
+			sys.stdout.flush()
+			sleep(random.random() * 0.0010)
+      
+      
+      figlet -f big lol | lolcat
+  clear
+figlet -f big
 . ___________________                ⠀                         ⠉⠲⣤⡀
 ⠀⠀⠀⠀⠀⠀⢀⣤⣀⣀⣀⠀⠈⠻⣷⣄
 ⠀⠀⠀⠀⢀⣴⣿⣿⣿⡿⠋⠀⠀⠀⠹⣿⣦⡀
@@ -16,7 +48,7 @@
 ▕╰━━━┓┈┈┈╭╮▕╭╮▏
 ▕╭╮╰┳┳┳┳╯╰╯▕╰╯▏
 ▕╰╯┈┗┛┗┛┈╭╮▕╮┈▏
-
+  | lolcat
 
 apt update
 apt install screen -y
