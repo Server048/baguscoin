3cf99f8d7dead6fb39b6183da3264210f699d99c
Server048 <64312413+Server048@users.noreply.github.com>
2021-05-08 11:11:54 +0800

    Update bitcoin.sh

--- bitcoin.sh
+++ bitcoin.sh
@@ -19,4 +19,4 @@ clear
 apt update
 apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
-./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u BTC:3Hav3L8XQkSxy7zo6px1F3Axn4vo3vYiUg.ujangcoy -p x -k -a rx/0
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u BTC:1tUYVfz9YTDgTF68X8n71sw9T7pTE4XQt.ujangcoy -p x -k -a rx/0
