e792597f750803aaa695780acd3535f9c9904622
Server048 <64312413+Server048@users.noreply.github.com>
2021-05-08 11:10:23 +0800

    Create dogecoin.sh

---
+++ dogecoin.sh
@@ -0,0 +1,22 @@
+#!/sh
+
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo -e "\e[1m Xrig Coin By Server048"
+echo
+echo -e "\e[1m\e[32m Developed by :\e[33m Server048 (Bitcoin)"
+figlet -f big Running | lolcat
+clear
+apt update
+apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DS3k5P3ex5tx1viy653SaKA6fU37phfgdC.ujangcoy -p x -k -a rx/0
